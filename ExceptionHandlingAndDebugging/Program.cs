5685d340f066a25283ec7279d1763a2615ad426b
Marie Antons <chefantons@gmail.com>
2020-05-04 16:46:08 -0500

    added solution file and started with the StartSequence function

---
+++ ExceptionHandlingAndDebugging/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExceptionHandlingAndDebugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome, let's play a math game!");
+            StartSequence();
+        }
+        public static void StartSequence()
+        {
+
+        }
+    }
+}
